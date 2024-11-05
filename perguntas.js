eb8046cd69e9e512300d437390ad0f8f450426e1
KDAVI77777 <00001096813117sp@al.educacao.sp.gov.br>
2024-11-05 10:45:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'TITULOS 2003',
+    'Quantos titulos foram conquistados no ano da tríplece coroa ?',
+    'Foram os titulos do Campeonato Mineiro,Copa do Brasil e Brasileirão sendo esse o primeiro Brasileirão de pontos corridos'
+)
+
+criaCartao(
+    'FINAL LIBERTADORES 1976',
+    'Quem foi o adversario do Cruzeiro na final da COPA LIBERTADORES 1976?',
+    'FOI O RIVER PLATE! Após vencer o primeiro jogo por 4 a 1, o Cruzeiro foi a Argentina e conheceu sua única derrota na competição, perdendo por 2 a 1 para o clube argentino.'
+)
+
+criaCartao(
+    'REI DE COPAS',
+    'Quantas Copas do Brasil tem o Cruzeiro?',
+    'São 6 titulos ao total esses conquistados em 1993 / 1996 / 2000 / 2003 / 2017 e 2018 tornando-se hexacampeão, e em 2018 o primeiro a conquistar a taça por dois anos consecutivos. Com a conquista do hexa o Cruzeiro torna-se o maior campeão da historia da Copa do Brasil.'
+)
+
+criaCartao(
+    'ANO DE FUNDAÇÃO',
+    'Em qual ano foi fundado o Cruzeiro Esporte Clube ?',
+    'Fundado em 1921 com o nome de Società Sportiva Palestra Italia, foi rebatizado para seu nome atual em 1942 - em referência ao Cruzeiro do Sul - por imposição do governo federal à época proibiu o uso no país de quaisquer símbolos de Alemanha, Itália e Japão, nações inimigas do Brasil no contexto da Segunda Guerra Mundial.'
+)
